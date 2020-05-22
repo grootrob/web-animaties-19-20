39d2a9a9b03057d5dca4e4bdc689a9f080874ea3
Rob de Groot <groot_rob@live.nl>
2020-05-22 22:32:15 +0200

    22 mei

--- follow.js
+++ follow.js
@@ -1,31 +1,28 @@
-(function () {
-    /*************/
-    /* follow me */
-    /*************/
-    let followMe = document.querySelector("#modern");
-
-    followMe.onmousemove = handleMouseMove;
-
-    function handleMouseMove(event) {
-        let box = followMe.getBoundingClientRect();
-
-        let body = document.body;
-        let docEl = document.documentElement;
-
-        let scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
-        let scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
-
-        let clientTop = docEl.clientTop || body.clientTop || 0;
-        let clientLeft = docEl.clientLeft || body.clientLeft || 0;
-
-        let top = box.top + scrollTop - clientTop;
-        let left = box.left + scrollLeft - clientLeft;
-
-        let relX = event.pageX - Math.round(left);
-        let relY = event.pageY - Math.round(top);
-
-        document.documentElement.style.setProperty('--midX', relX + 'px');
-        document.documentElement.style.setProperty('--midY', relY + 'px');
-    }
-
-})
+//window.onload = function () {
+//
+//    var parallaxBox = document.getElementById('section2');
+//    var c1left = document.getElementsByClassName('cls-1').offsetLeft,
+//        c1top = document.getElementsByClassName('cls-12').offsetTop,
+//        c2left = document.getElementsByClassName('cls-3').offsetLeft;
+//
+//    parallaxBox.onmousemove = function (event) {
+//        event = event || window.event;
+//        var x = event.clientX - parallaxBox.offsetLeft,
+//            y = event.clientY - parallaxBox.offsetTop;
+//
+//        mouseParallax('cls-1', c1left, c1top, x, y, 5);
+//        mouseParallax('cls-2', c2left, c2top, x, y, 15);
+//        mouseParallax('cls-3', c3left, c3top, x, y, 30);
+//
+//    }
+//
+//}
+//
+//function mouseParallax(id, left, top, mouseX, mouseY, speed) {
+//    var obj = document.getElementById(id);
+//    var parentObj = obj.parentNode,
+//        containerWidth = parseInt(parentObj.offsetWidth),
+//        containerHeight = parseInt(parentObj.offsetHeight);
+//    obj.style.left = left - (((mouseX - (parseInt(obj.offsetWidth) / 2 + left)) / containerWidth) * speed) + 'px';
+//    obj.style.top = top - (((mouseY - (parseInt(obj.offsetHeight) / 2 + top)) / containerHeight) * speed) + 'px';
+//}
